4db9f986e697b5d2d9ac1f86c16bb59cc5e96e39
thomaswerner35 <thomaswerner35@1aae85c6-19f3-460c-a681-f389938dc47c>
2012-01-31 18:29:16 +0000

    Changed license from LGPL to GPL
    git-svn-id: svn://svn.code.sf.net/p/nbbeanshell/code@28 1aae85c6-19f3-460c-a681-f389938dc47c

--- trunk/src/de/bfg9000/beanshell/completion/CompletionQueryItemProvider.java
+++ trunk/src/de/bfg9000/beanshell/completion/CompletionQueryItemProvider.java
@@ -2,15 +2,15 @@
  * nbBeanShell -- a integration of BeanShell into the NetBeans IDE
  * Copyright (C) 2012 Thomas Werner
  *
- * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
- * Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any
- * later version.
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later
+ * version.
  *
  * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
  * details.
  *
- * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+ * You should have received a copy of the GNU General Public License along with this library; if not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 package de.bfg9000.beanshell.completion;
